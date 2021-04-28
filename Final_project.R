d9c4a5e11fa33f85578befed9cb765ddab8ce76a
Hans <hjp141@hotmail.com>
2021-04-28 11:59:59 -0500

    being more specific

--- Final_project.R
+++ Final_project.R
@@ -1,16 +1,29 @@
 library('readxl')
 gom <- read_xlsx("C:/Users/hjp4906/Desktop/Final Project/GOMECC-3 Cruise Data.xlsx")
 unique(gom$DEPTH_METER)
+
+#getting rid of first row
 gom <- gom[-1,]
+
+#turning columns into numeric so i can edit and getting rid of unnecessary columns
 gom$DEPTH_METER <- as.numeric(gom$DEPTH_METER)
-which(colnames(gom)=="DEPTH_METER")
-col2keep <- c("Sample_ID", "EXPOCODE", "CRUISE_ID", "CAST_NO", "DATE_UTC", "TIME_UTC", 'LATITUDE_DEC', 'LONGITUDE_DEC', 
-              'DEPTH_METER', 'BOTDEPTH', 'PCO2_MEA_UATM', 'CHLA_MG_M3')
-surf_gom <- gom[gom$DEPTH_METER <= 20,]
-surf_gom <- surf_gom[,col2keep]
 surf_gom$LATITUDE_DEC <- as.numeric(surf_gom$LATITUDE_DEC)
 surf_gom$LONGITUDE_DEC <- as.numeric(surf_gom$LONGITUDE_DEC)
 surf_gom$BOTDEPTH <- as.numeric(surf_gom$BOTDEPTH)
 surf_gom$PCO2_MEA_UATM <- as.numeric(surf_gom$PCO2_MEA_UATM)
 surf_gom$CHLA_MG_M3 <- as.numeric(surf_gom$CHLA_MG_M3)
+which(colnames(gom)=="DEPTH_METER")
+col2keep <- c("Sample_ID", "EXPOCODE", "CRUISE_ID", "CAST_NO", "DATE_UTC", "TIME_UTC", 'LATITUDE_DEC', 'LONGITUDE_DEC', 
+              'DEPTH_METER', 'BOTDEPTH', 'PCO2_MEA_UATM', 'CHLA_MG_M3')
+surf_gom <- surf_gom[,col2keep]
+
+#getting rid of anything below 20 m
+surf_gom <- gom[gom$DEPTH_METER <= 20,]
+
+#getting rid of the -999 values
 surf_gom <- surf_gom[surf_gom$PCO2_MEA_UATM != -999,]
+
+#calculating total area of the chl_a 
+la <- read.csv("C:/Users/hjp4906/Desktop/louisiana.csv")
+sum(la$Shape_Area)
+56106289218-17702794135
